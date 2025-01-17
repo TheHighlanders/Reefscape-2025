7100057b8e3e53ca701f8bbc504da60fc2bf0e55
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2025-01-17 16:49:49 -0500

    elevator work description has other info
    elevatorbegun with some code coppied from https://github.com/REVrobotics/REVLib-Examples/blob/main/Java/SPARK/Limits/src/main/java/frc/robot/Robot.java#L25

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,19 +9,73 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Superstructure.IntakeState;
 import frc.robot.utils.StateRequest;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.LimitSwitchConfig.Type;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+
+  private SparkMax motor;
+  private SparkMaxConfig motorConfig;
+  private SparkLimitSwitch forwardLimitSwitch;
+  private SparkLimitSwitch reverseLimitSwitch;
+  private RelativeEncoder encoder;
+
+
+
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+
+    motor = new SparkMax(1, MotorType.kBrushless);
+    forwardLimitSwitch = motor.getForwardLimitSwitch();
+    reverseLimitSwitch = motor.getReverseLimitSwitch();
+    encoder = motor.getEncoder();
+
+    motorConfig = new SparkMaxConfig();
+
+    motorConfig.idleMode(IdleMode.kBrake);
+
+    motorConfig.limitSwitch
+    .forwardLimitSwitchType(Type.kNormallyOpen)
+    .forwardLimitSwitchEnabled(true)
+    .reverseLimitSwitchType(Type.kNormallyOpen)
+    .reverseLimitSwitchEnabled(true);
+
+    motorConfig.softLimit
+    .forwardSoftLimit(50)
+    .forwardSoftLimitEnabled(true)
+    .reverseSoftLimit(-50)
+    .reverseSoftLimitEnabled(true);
+
+    motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+    // Reset the position to 0 to start within the range of the soft limits
+    encoder.setPosition(0);
+
+    // Initialize dashboard values
+    SmartDashboard.setDefaultBoolean("Direction", true);
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+
+        // Display data from SPARK onto the dashboard
+        SmartDashboard.putBoolean("Forward Limit Reached", forwardLimitSwitch.isPressed());
+        SmartDashboard.putBoolean("Reverse Limit Reached", reverseLimitSwitch.isPressed());
+        SmartDashboard.putNumber("Applied Output", motor.getAppliedOutput());
+        SmartDashboard.putNumber("Position", encoder.getPosition());
   }
 
   @Override
@@ -62,6 +116,13 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+
+        // Set the motor setpoint based on the direction from the dashboard
+        if (SmartDashboard.getBoolean("Direction", true)) {
+          motor.set(0.2);
+        } else {
+          motor.set(-0.2);
+        }
   }
 
   @Override
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,16 +4,42 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 // PLACEHOLDER FOR SUPERSTRUCTURE EXAMPLE
+
+
 
 public class Elevator extends SubsystemBase {
   /** Creates a new Elevator. */
-  public Elevator() {}
+  public enum ElevatorState {
+    HOME,
+    L1_POSITION,
+    L2_POSITION,
+    L3_POSITION,
+    L4_POSITION,
+    CORAL_POSITION
+  }
+  private ElevatorState uppydowny = ElevatorState.HOME;
+
+  public Elevator() {
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    switch (uppydowny) {
+      case HOME:
+        break;
+      case L1_POSITION:
+        break;
+      case L2_POSITION:
+        break;
+      case L3_POSITION:
+        break;
+        case L4_POSITION:
+        break;
+    }
   }
 }
