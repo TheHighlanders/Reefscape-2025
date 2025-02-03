b455f62e4fb4e53caf586fbba6d66bb6c7ca6296
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2025-02-03 15:46:47 -0500

    Elevator Cleanup before merge

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,15 +4,12 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// PLACEHOLDER FOR SUPERSTRUCTURE EXAMPLE
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-// import frc.robot.subsystems.Superstructure.IntakeState;
-// import frc.robot.utils.StateRequest;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -25,20 +22,35 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.LimitSwitchConfig.Type;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard; ///// idk if this needs to be here but i think this is for the thing we use to monitor robot happenings
+
+class elevatorConstants {
+  static final int elevMotorID = 40;
+
+  static final double elevPCF = 1;
+
+  static final double homeTarget = 0;
+  static final double l1Target = 0;
+  static final double l2Target = 10;
+  static final double l3Target = 20;
+  static final double l4Target = 30;
+  static final double coral_positionTarget = 40;
+
+  static final double feedFoward = 8;
+  static final double elevP = 1;
+  static final double elevI = 0;
+  static final double elevD = 0;
+
+  static final double forwardSoftLimit = 50;
+  static final double backwardSoftLimit = 0;
+
+  static final double[] outputRange = { -1d, 1d };
+
+  static final double maxVelocity = 5;
+  static final double maxAccel = 5;
+  static final double maxClosedLoopError = 5;
+}
 
 public class Elevator extends SubsystemBase {
-
-  private SparkMax elevatorMotor;
-  private SparkMaxConfig elevatorMotorConfig;
-  private SparkLimitSwitch reverseLimitSwitch;
-  private RelativeEncoder elevatorEncoder;
-
-  SparkClosedLoopController m_controller;
-
-  private double feedFoward = 8;
-
- 
   public enum ElevatorState { //Creates a new Elevator.
     HOME,
     L1_POSITION,
@@ -48,57 +60,60 @@ public class Elevator extends SubsystemBase {
     CORAL_POSITION
   }
 
-  double homeTarget = 0;
-  double l1Target = 0;
-  double l2Target = 10;
-  double l3Target = 20;
-  double l4Target = 30;
-  double coral_positionTarget = 40;
+  private SparkMax elevatorMotor;
+  private SparkMaxConfig elevatorMotorConfig;
+  private SparkLimitSwitch reverseLimitSwitch;
+  private RelativeEncoder elevatorEncoder;
+
+  SparkClosedLoopController m_controller;
 
   double targetPosition;
-
 
   private ElevatorState uppydowny = ElevatorState.HOME;
 
   public Elevator() {
     elevatorMotorConfig = new SparkMaxConfig();
-    elevatorMotor = new SparkMax(1, MotorType.kBrushless);
+    elevatorMotor = new SparkMax(elevatorConstants.elevMotorID, MotorType.kBrushless);
     reverseLimitSwitch = elevatorMotor.getReverseLimitSwitch();
-    elevatorEncoder = elevatorMotor.getEncoder();//TODO:Encoder should be configured (position conversion factor), based on cad, in order to use the feedback
+
+    elevatorMotorConfig.encoder
+        .positionConversionFactor(elevatorConstants.elevPCF)
+        .velocityConversionFactor(elevatorConstants.elevPCF / 60.0d);
 
     m_controller = elevatorMotor.getClosedLoopController();
     elevatorMotorConfig.idleMode(IdleMode.kBrake);
+
     elevatorMotorConfig.limitSwitch
         .reverseLimitSwitchType(Type.kNormallyOpen)
         .reverseLimitSwitchEnabled(true);
 
     elevatorMotorConfig.softLimit
-        .forwardSoftLimit(50)  // TODO: chanege limet value
+        .forwardSoftLimit(elevatorConstants.forwardSoftLimit) // TODO: chanege limit value
         .forwardSoftLimitEnabled(true)
-        .reverseSoftLimit(0)
+        .reverseSoftLimit(elevatorConstants.backwardSoftLimit)
         .reverseSoftLimitEnabled(true);
 
     SparkMaxConfig config = new SparkMaxConfig();
 
     // Set Smart Motion and Smart Velocity parameters.
     elevatorMotorConfig.closedLoop.maxMotion
-        .maxVelocity(5) // TODO find these desigherd values
-        .maxAcceleration(5)
-        .allowedClosedLoopError(5);  
+        .maxVelocity(elevatorConstants.maxVelocity) // TODO find these desirerd values
+        .maxAcceleration(elevatorConstants.maxAccel)
+        .allowedClosedLoopError(elevatorConstants.maxClosedLoopError);
     // Set PID gains
-    config.closedLoop // pid loop to contoll elevator elevating rate
-        .p(.1)
-        .i(.029) // TODO find these desigherd values
-        .d(.01)
-        .outputRange(4, 5);
+    config.closedLoop // pid loop to control elevator elevating rate
+        .p(elevatorConstants.elevP)
+        .i(elevatorConstants.elevI) // TODO find these desirerd values
+        .d(elevatorConstants.elevD)
+        .outputRange(elevatorConstants.outputRange[0], elevatorConstants.outputRange[1]);
 
     elevatorMotor.configure(elevatorMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
+    //TODO: Encoder should be configured (position conversion factor), based on cad, in order to use the feedback
+    elevatorEncoder = elevatorMotor.getEncoder();
+
     // Reset the position to 0 to start within the range of the soft limits
     elevatorEncoder.setPosition(0);
-
-
-
   }
 
   @Override
@@ -111,80 +126,69 @@ public class Elevator extends SubsystemBase {
 
     switch (uppydowny) {
       case HOME:
-        targetPosition = homeTarget;                                                                            // the set point
+        targetPosition = elevatorConstants.homeTarget; // the set point
         break;
       case L1_POSITION:
-        targetPosition = l1Target;                                                                                  // the set point
+        targetPosition = elevatorConstants.l1Target; // the set point
         break;
       case L2_POSITION:
-         targetPosition = l2Target;                                                                                                // the set point
+        targetPosition = elevatorConstants.l2Target; // the set point
         break;
       case L3_POSITION:
-        targetPosition = l3Target;                                                                                     // the set point
+        targetPosition = elevatorConstants.l3Target; // the set point
         break;
       case L4_POSITION:
-        targetPosition = l4Target;                                                                                    // the set point
+        targetPosition = elevatorConstants.l4Target; // the set point
         break;
       case CORAL_POSITION:
-        targetPosition = coral_positionTarget;                                                                                    // the set point
+        targetPosition = elevatorConstants.coral_positionTarget; // the set point
         break;
     }
-    m_controller.setReference(targetPosition, ControlType.kPosition, ClosedLoopSlot.kSlot0, feedFoward);
+    m_controller.setReference(targetPosition, ControlType.kPosition, ClosedLoopSlot.kSlot0,
+        elevatorConstants.feedFoward);
   }
-  public void L1(){
+
+  public void L1() {
     uppydowny = ElevatorState.L1_POSITION;
   }
-  public void L2(){
+
+  public void L2() {
     uppydowny = ElevatorState.L2_POSITION;
   }
-  public void L3(){
+
+  public void L3() {
     uppydowny = ElevatorState.L3_POSITION;
   }
-  public void L4(){
+
+  public void L4() {
     uppydowny = ElevatorState.L4_POSITION;
   }
-  public void HOME(){
+
+  public void HOME() {
     uppydowny = ElevatorState.HOME;
   }
-  public void coral_position(){
+
+  public void coral_position() {
     uppydowny = ElevatorState.CORAL_POSITION;
   }
 
   public Command getL1Command() {
-    return new RunCommand(
-      ()->{
-        L1();
-      })
-      .finallyDo(()-> L1());
-    }  
+    return new InstantCommand(this::L1);
+  }
+
   public Command getL2Command() {
-      return new RunCommand(
-      ()->{
-        L2();
-      })
-      .finallyDo(()-> L2());
-    } 
+    return new InstantCommand(this::L2);
+  }
+
   public Command getL3Command() {
-      return new RunCommand(
-    ()->{
-      L3();
-    })
-      .finallyDo(()-> L3());
-    } 
+    return new InstantCommand(this::L3);
+  }
+
   public Command getL4Command() {
-      return new RunCommand(
-    ()->{
-      L4();
-    })
-      .finallyDo(()-> L4());
-    } 
-  public Command getCoral_positoonCommand() {
-      return new RunCommand(
-    ()->{
-      coral_position();
-    })
-      .finallyDo(()-> coral_position());
-    } 
+    return new InstantCommand(this::L4);
+  }
 
+  public Command getCoral_positionCommand() {
+    return new InstantCommand(this::coral_position);
+  }
 }
-
