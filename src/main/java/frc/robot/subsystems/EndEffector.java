c8c054552cbf294d88744db3f4fed33b59bab292
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2025-02-05 13:21:22 -0500

    Update EndEffector.java

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -2,8 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// in this there will be a motor, a beam brake
-
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -14,7 +12,6 @@ import com.revrobotics.spark.SparkSim;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -23,7 +20,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
 final class endEffectorConstants {
   static final int intakePhotoSensorDIOPin = 0;
@@ -55,18 +51,6 @@ public class EndEffector extends SubsystemBase {
   public EndEffector() {
     photoSensor = new DigitalInput(endEffectorConstants.intakePhotoSensorDIOPin);
     effector = new SparkMax(endEffectorConstants.motorID, MotorType.kBrushless);
-    effectorNeo = DCMotor.getNEO(1);
-    effectorNeoSim =
-        new DCMotorSim(
-            LinearSystemId.createDCMotorSystem(
-                effectorNeo,
-                endEffectorConstants.simulation.effectorMOI,
-                endEffectorConstants.simulation.effectorPCF),
-            effectorNeo);
-
-    if (Constants.sim) {
-      effectorSim = new SparkSim(effector, effectorNeo);
-    }
 
     SparkMaxConfig effectorConfig = effectorConfg();
 
@@ -93,24 +77,24 @@ public class EndEffector extends SubsystemBase {
     return photoSensor.get();
   }
 
-  public void intakeStop() {
-    DriverStation.reportWarning("Intake stop", false);
+  public void effectorStop() {
+    DriverStation.reportWarning("Effector stop", false);
     effector.set(0);
   }
 
   public void effectorForward() {
-    DriverStation.reportWarning("Intake forward", false);
+    DriverStation.reportWarning("Effector forward", false);
     effector.set(1.0);
+  }
+
+  public void effectorReverse() {
+    DriverStation.reportWarning("Effector reverse", false);
+    effector.set(-1.0);
   }
 
   public Command effectorForwardUntilBrakeCMD() {
     return new RunCommand(this::effectorForward, this)
-        .finallyDo(this::intakeStop)
+        .finallyDo(this::effectorStop)
         .until(this::hasGamePiece);
-  }
-
-  public void effectorReverse() {
-    DriverStation.reportWarning("Intake reverse", false);
-    effector.set(-1.0);
   }
 }
