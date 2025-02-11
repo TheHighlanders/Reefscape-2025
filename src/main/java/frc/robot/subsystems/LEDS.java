fcde4a9aba7eb377188761021d68c5f061354915
potsofbeans <118775526+potsofbeans@users.noreply.github.com>
2025-02-10 19:08:28 -0500

    branch begun

---
+++ src/main/java/frc/robot/subsystems/LEDS.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+//************************************************************************************
+// REFRENCE: https://docs.wpilib.org/en/stable/docs/software/hardware-apis/misc/addressable-leds.html
+//************************************************************************************
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.config.ClosedLoopConfigAccessor;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.LEDReader;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Elevator.ElevatorState;
+
+public class LEDS extends SubsystemBase {
+  private static final int kPort = 9;
+  private static final int kLength = 120;
+
+  private final AddressableLED m_led;
+  private final AddressableLEDBuffer m_buffer;
+
+
+    public enum LEDState {
+      RED,
+      BLUE,
+      RAINBOW
+    }
+
+    private LEDState ledsState = LEDState.RAINBOW;
+
+
+  public LEDS() {
+    m_led = new AddressableLED(kPort);
+    m_buffer = new AddressableLEDBuffer(kLength);
+    m_led.setLength(kLength);
+    m_led.start();
+
+    // Set the default command to turn the strip off, otherwise the last colors written by
+    // the last command to run will continue to be displayed.
+    // Note: Other default patterns could be used instead!
+    setDefaultCommand(runPattern(LEDPattern.solid(Color.kBlack)).withName("Off"));
+
+  }
+
+  @Override
+  public void periodic() {
+    // Periodically send the latest LED color data to the LED strip for it to display
+    m_led.setData(m_buffer);
+    
+    switch (ledsState) {
+      case RAINBOW:
+        break;
+      case RED:
+        break;
+      case BLUE:
+        break;
+
+    }
+  }
+
+  /**
+   * Creates a command that runs a pattern on the entire LED strip.
+   *
+   * @param pattern the LED pattern to run
+   */
+  public Command runPattern(LEDPattern pattern) {
+    return run(() -> pattern.applyTo(m_buffer));
+  }
+}
