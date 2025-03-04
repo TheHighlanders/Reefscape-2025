42308c9762c81b241ea9be0d703633eb402a8261
potsofbeans <118775526+potsofbeans@users.noreply.github.com>
2025-03-04 17:37:44 -0500

    wip

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -4,14 +4,46 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
 public class LEDs extends SubsystemBase {
-  /** Creates a new LEDs. */
-  public LEDs() {}
+  private static final int kPort = 9;
+  private static final int kLength = 120;
+
+  private final AddressableLED m_led;
+  private final AddressableLEDBuffer m_buffer;
+
+  public LEDSubsystem() {
+    m_led = new AddressableLED(kPort);
+    m_buffer = new AddressableLEDBuffer(kLength);
+    m_led.setLength(kLength);
+    m_led.start();
+
+    // Set the default command to turn the strip off, otherwise the last colors written by
+    // the last command to run will continue to be displayed.
+    // Note: Other default patterns could be used instead!
+    //setDefaultCommand(runPattern(LEDPattern.solid(Color.kBlack)).withName("Off"));
+    m_led.runPattern(LEDPattern.solid(Color.kWhite);
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // Periodically send the latest LED color data to the LED strip for it to display
+    m_led.setData(m_buffer);
+  }
+
+  /**
+   * Creates a command that runs a pattern on the entire LED strip.
+   *
+   * @param pattern the LED pattern to run
+   */
+  public Command runPattern(LEDPattern pattern) {
+    return run(() -> pattern.applyTo(m_buffer));
   }
 }
