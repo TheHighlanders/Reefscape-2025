947c3b18acab11b8394ca37e83513741a89a4201
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2025-03-12 18:18:40 -0400

    Remove JVM args

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Reefscape-2025";
+  public static final String VERSION = "1.0.0";
+  public static final int GIT_REVISION = 347;
+  public static final String GIT_SHA = "85afac1b13e15bd26473f28a581127f8577541e7";
+  public static final String GIT_DATE = "Mar 12, 2025 4:40:59 PM EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "Mar 12, 2025 6:16:32 PM EDT";
+  public static final long BUILD_UNIX_TIME = 1741817792680L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
