e8398252d2cd38b0a0a47ac8890a1cf786da3082
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2025-03-29 17:49:37 -0400

    Update at 'Sat Mar 29 17:49:37 EDT 2025'

--- src/main/java/frc/robot/subsystems/Module.java
+++ src/main/java/frc/robot/subsystems/Module.java
@@ -55,7 +55,7 @@ final class ModuleConstants {
 
   static final double anglePCF = 360.0 / 12.8d;
 
-  static final int driveCurrentLimit = 50;
+  static final int driveCurrentLimit = 45;
   static final int angleCurrentLimit = 40;
 
   static final boolean absolInverted = false;
