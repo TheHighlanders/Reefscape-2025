5bc1f216fec4ddb7aa8ed720e689f8531b0a2305
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2025-03-31 17:27:06 -0400

    Update at 'Mon Mar 31 17:27:06 EDT 2025'

--- src/main/java/frc/robot/commands/Align.java
+++ src/main/java/frc/robot/commands/Align.java
@@ -49,7 +49,7 @@ public class Align extends Command {
     static final double positionTolerance = 0.025;
 
     // Velocity tolerance (meters/s)
-    static final double velocityTolerance = 0.05;
+    static final double velocityTolerance = 0.1;
 
     // Rotation tolerance (radians)
     static final double rotationTolerance = Units.degreesToRadians(5);
