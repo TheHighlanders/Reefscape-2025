106e92d8b6cad2104fc5c62a6c005f7dbca5c8c3
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2025-03-31 15:28:44 -0400

    Update at 'Mon Mar 31 15:28:43 EDT 2025'

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -48,7 +48,7 @@ final class VisionConstants {
       new Transform3d(
           new Translation3d(0.20446, -0.12066, 0.40855),
           new Rotation3d(
-              Units.degreesToRadians(0), Units.degreesToRadians(15.3), Units.degreesToRadians(35)));
+              Units.degreesToRadians(0), Units.degreesToRadians(20), Units.degreesToRadians(35)));
 
   static final Transform3d reefLeft =
       new Transform3d(
